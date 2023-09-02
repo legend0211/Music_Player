ce701706109c06db78dc0aebf131d9a76bd65d73
SOHAM SEN 2001 <soham2001sen@gmail.com>
2023-09-02 21:27:46 +0530

    Second Commit

--- app/src/main/java/com/example/music_player/MainActivity.java
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ImageView imgv = findViewById(R.id.Search);
         imgv.setOnClickListener(new View.OnClickListener() {
             @Override
