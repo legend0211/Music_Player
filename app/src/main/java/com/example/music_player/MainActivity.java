4028770b51ddfd79f6237f29cefbfd4ec37ed062
SOHAM SEN 2001 <soham2001sen@gmail.com>
2023-09-04 02:01:38 +0530

    Added Song List, Song name, Artist name, Seeker

--- app/src/main/java/com/example/music_player/MainActivity.java
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.music_player;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,25 +26,93 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import android.Manifest;
+import static android.content.ContentValues.TAG;
+import static android.provider.Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+
+
 public class MainActivity extends AppCompatActivity {
+    private final int READ_STORAGE_PERMISSION_REQUEST = 1;
+    ListView songListView;
+    ImageView imgv;
+    File[] songFolderFiles;
+    ArrayList<String> nameOfSongs;
+    HashMap<String, Song> map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestStoragePermission();
 
-        ImageView imgv = findViewById(R.id.Search);
+        initialisations();
+        clickables();
 
-        imgv.setOnClickListener(new View.OnClickListener() {
+        getSongs();
+    }
+
+    @AfterPermissionGranted(READ_STORAGE_PERMISSION_REQUEST)
+    public void requestStoragePermission() {
+        String perms = Manifest.permission.READ_EXTERNAL_STORAGE;
+        if(EasyPermissions.hasPermissions(this, perms)) {
+            //Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            EasyPermissions.requestPermissions(this, "Please grant the storage permission", READ_STORAGE_PERMISSION_REQUEST, perms);
+        }
+    }
+
+    public void initialisations() {
+        imgv = findViewById(R.id.Search);
+        songListView = findViewById(R.id.songsListView);
+
+        nameOfSongs = new ArrayList<>();
+        map = new HashMap<>();
+    }
+
+    public void clickables() {
+        songListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SongActivity.class);
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("Entered Song");
+                String songName = (String)parent.getItemAtPosition(position);
+                if(map.containsKey(songName)) {
+                    Intent intent = new Intent(MainActivity.this, SongActivity.class);
+                    intent.putExtra("artistName", map.get(songName).artist);
+                    intent.putExtra("songName", map.get(songName).name);
+                    intent.putExtra("songPath", map.get(songName).path);
+                    startActivity(intent);
+                }
             }
         });
+    }
 
-        ListView songListView = findViewById(R.id.songsListView);
+    public void getSongs() {
+        File songFolder = new File(Environment.getExternalStorageDirectory(), "SongFolder");
+        songFolderFiles = songFolder.listFiles();
+        if (songFolderFiles != null) {
+            for (File file : songFolderFiles) {
+                nameOfSongs.add(file.getName().substring(0,file.getName().length()-4));
+                Song song = new Song();
+                song.artist = "Anonymous";
+                song.name = nameOfSongs.get(nameOfSongs.size()-1);
+                song.path = file.getPath();
+                map.put(song.name, song);
+            }
+        }
 
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nameOfSongs);
+        songListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/music_player/SongActivity.java
+++ app/src/main/java/com/example/music_player/SongActivity.java
@@ -10,28 +10,128 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 public class SongActivity extends Activity {
 
-    private ImageView imagePlayPause;
-    private TextView textCurrentTime, textTotalDuration;
-    private SeekBar seekBar;
-    private MediaPlayer mediaPlayer;
-    private Handler handler;
-
+    ImageView imagePlayPause;
+    TextView textCurrentTime, textTotalDuration;
+    SeekBar seekBar;
+    MediaPlayer mediaPlayer;
+    Handler handler;
+    TextView textTitle, textArtist;
+    int duration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
+        initialisation();
+        clickables();
 
+        String artistName = getIntent().getStringExtra("artistName");
+        String songName = getIntent().getStringExtra("songName");
+        String songPath = getIntent().getStringExtra("songPath");
+        playSong(artistName, songName, songPath);
+    }
+
+    public void initialisation() {
         imagePlayPause = findViewById(R.id.buttonPlay);
         textCurrentTime = findViewById(R.id.textCurrentTime);
         textTotalDuration = findViewById(R.id.textTotalTime);
         seekBar = findViewById(R.id.playerSeekBar);
-        seekBar.setMax(100);
+        textTitle = findViewById(R.id.textTitle);
+        textArtist = findViewById(R.id.textArtist);
+
         mediaPlayer = new MediaPlayer();
         handler = new Handler();
-
     }
 
+    public void clickables() {
+        imagePlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("ImagePlayPause Clicked "+mediaPlayer.isPlaying());
+                if (mediaPlayer.isPlaying()) {
+                    duration = mediaPlayer.getCurrentPosition();
+                    mediaPlayer.pause();
+                    imagePlayPause.setImageResource(R.drawable.ic_play);
+                } else {
+                    mediaPlayer.start();
+                    mediaPlayer.seekTo(duration);
+                    imagePlayPause.setImageResource(R.drawable.ic_pause);
+                    updateSeekBar();
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    public void playSong(String artistName, String songName, String songPath) {
+        textTitle.setText(songName);
+        textArtist.setText(artistName);
+
+        // Create and configure a MediaPlayer to play the selected song
+        try {
+            mediaPlayer.setDataSource(songPath);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            textTotalDuration.setText(millisecondsToMinutesAndSeconds(mediaPlayer.getDuration()));
+            seekBar.setMax(mediaPlayer.getDuration());
+            textCurrentTime.setText("0:00");
+            updateSeekBar();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String millisecondsToMinutesAndSeconds(long milliseconds) {
+        int seconds = (int) (milliseconds / 1000);
+        int minutes = seconds / 60;
+        seconds %= 60;
+
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+
+    public void updateSeekBar() {
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+        textCurrentTime.setText(millisecondsToMinutesAndSeconds(mediaPlayer.getCurrentPosition()));
+        if (mediaPlayer.isPlaying()) {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    updateSeekBar();
+                }
+            };
+            handler.postDelayed(runnable, 500);
+        }
+        else {
+            imagePlayPause.setImageResource(R.drawable.ic_play);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+        handler.removeCallbacksAndMessages(null);
+    }
 }
